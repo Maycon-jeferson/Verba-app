65d26bd8da2c0357989302568d2398833c53095a
Maycon-jeferson <jefersonmaycon3@gmail.com>
2025-07-18 22:49:20 -0300

    Initial commit: VerbaApp SaaS project setup

---
+++ src/app/home/page.tsx
@@ -0,0 +1,26 @@
+export default function Dashboard() {
+  return (
+    <main className="p-6">
+      {/* cabecalho */}
+      <header>
+        <div>
+            <h1>cabecalho</h1>
+        </div>
+      </header>
+
+      {/* caixa de texto */}
+      <div> 
+        <div>
+            <h2>caixa de texto</h2>
+        </div>
+      </div>
+
+      {/* notas recentes */}
+      <div>
+        <div>
+            <h2>notas recentes</h2>
+        </div>
+      </div>
+    </main>
+  )
+}
